7f29b1a37a13e8bb1dc7051ae5b2258e9827164b
MinakovaLisa <giti528@ya.ru>
2021-03-19 13:56:08 +0300

    gameField normalization: created new form of gameField

--- src/business/initialState/index.ts
+++ src/business/initialState/index.ts
@@ -10,6 +10,7 @@ import {
   HealthItemType,
   State,
   GameField,
+  HealthCard,
 } from "../types";
 
 export const START_COORD = { hor: 0, vert: 0 };
@@ -46,11 +47,10 @@ const newGameField = {
   },
 };
 
-const getCellOrder = () => {
+const getCellOrder = (): Array<string> => {
   const width = FINISH_COORD.hor;
   const height = FINISH_COORD.vert;
 
-  // TODO: нужно выносить в отдельный тип-?!
   let orderList: Array<string> = [];
 
   for (let hor = 0; hor <= width; hor++) {
@@ -59,6 +59,7 @@ const getCellOrder = () => {
       orderList.push(index);
     }
   }
+
   return orderList;
 };
 
@@ -69,6 +70,7 @@ const createEmptyGameField = (cellList: Array<string>): GameField => {
   };
 
   let newEmptyGameField: GameField = {};
+
   cellList.map((cell: string) => {
     newEmptyGameField[cell] = emptyFieldItem;
   });
@@ -113,62 +115,137 @@ const organizeGameField = (emptyField: GameField): GameField => {
   return organizedGameField;
 };
 
-const getRandomNumber = (arrNumber: Array<number>): number => {
-  const number = Math.floor(Math.random() * 30);
+const getRandomNumber = (
+  arrNumber: Array<number>,
+  maxNumber: number
+): number => {
+  const number = Math.floor(Math.random() * 90);
 
   if (arrNumber) {
-    return arrNumber.includes(number) ? getRandomNumber(arrNumber) : number;
+    return arrNumber.includes(number)
+      ? getRandomNumber(arrNumber, maxNumber)
+      : number;
   } else {
     return number;
   }
 };
 
-const getCellForCards = (cellList: Array<string>) => {
-  const keyList = new Array(30).fill(0).reduce((prev) => {
-    const index = getRandomNumber(prev);
-    if (prev) {
-      return [...prev, index];
-    } else return [index];
+const getListForCards = (gameField: GameField): [string, СommonCell][] => {
+  const listGameField = Object.entries(gameField);
+  const emptyCellsList = listGameField.filter((cellItem): cellItem is [
+    string,
+    СommonCell
+  ] => {
+    const [index, item] = cellItem;
+    return item.name === "commonCell";
   });
 
-  const cellForCards = keyList.map((keyItem: number) => {
-    return cellList[keyItem];
+  const AMOUNT_EMPTY_CELLS = emptyCellsList.length;
+
+  const keyList: Array<number> = new Array(AMOUNT_HEALTH_ITEMS)
+    .fill(0)
+    .reduce((prevkeyList) => {
+      const randomNumber = getRandomNumber(prevkeyList, AMOUNT_EMPTY_CELLS);
+
+      if (prevkeyList) {
+        return [...prevkeyList, randomNumber];
+      } else {
+        return [randomNumber];
+      }
+    }, []);
+
+  const listForCards = keyList.map((keyItem: number): [string, СommonCell] => {
+    return emptyCellsList[keyItem];
   });
 
-  return cellForCards;
+  return listForCards;
 };
 
-const spreadHealthCards = (
-  gameField: GameField,
-  cellOrder: Array<string>
-) /* : GameField  */ => {
+const setHealthCards = (
+  cellList: [string, СommonCell][],
+  gameField: GameField
+): GameField => {
+  const cellListWithCards = cellList.map(
+    (cellWithCards: [string, СommonCell]) => {
+      const [index, cell] = cellWithCards;
+
+      const healthItem: HealthCard = {
+        name: "health",
+        type: getRandomType(),
+        apperance: "closed",
+      };
+
+      const cellWithCard = {
+        ...cell,
+        cardItem: { ...cell.cardItem, healthItem },
+      };
+      return [index, cellWithCard];
+    }
+  );
+
+  const gameFieldWithCards = Object.fromEntries(cellListWithCards);
+  const gameFieldFull = { ...gameField, ...gameFieldWithCards };
+
+  return gameFieldFull;
+};
+
+const getRandomType = (): HealthItemType => {
+  return HEALTH_ITEM_TYPE_ARR[Math.floor(Math.random() * 2)];
+};
+
+const spreadHealthCards = (gameField: GameField): GameField => {
   /**
-   * 1. @emptyCell сначала выберем индексы пустых ячеек
-   * 2. @getCellForCards выберем n-неповторяющихся ячеек
-   * 3. разложим на них карточки здоровья
+   * 1. cellsForCards вернет список ячеек для карточек
+   * 2. setHealthCardsразложим на них карточки здоровья
    */
 
-  const emptyCell = cellOrder.filter((cellIndex) => {
-    return gameField[cellIndex].name === "commonCell";
+  const cellsForCards = getListForCards(gameField);
+  const filledGameField = setHealthCards(cellsForCards, gameField);
+  return filledGameField;
+};
+
+const setPlayerCards = (gameField: GameField) /* : GameField */ => {
+  const startIndex = `${START_COORD.hor}.${START_COORD.vert}`;
+
+  const playerList = new Array(AMOUNT_PLAYERS).fill(0).map((item, index) => {
+    return {
+      name: "player",
+      health: INITIAL_PLAYER_HEALTH,
+      orderNumber: index,
+    };
   });
-  console.log(emptyCell);
-  const cellsForCards = getCellForCards(emptyCell);
-  console.log(cellsForCards);
+
+  const startCell = gameField[startIndex];
+  if (startCell.name === "start") {
+    const gameFieldFull = {
+      ...gameField,
+      [startIndex]: {
+        ...startCell,
+        cardItem: {
+          ...startCell.cardItem,
+          playerList,
+        },
+      },
+    };
+    return gameFieldFull;
+  }
 };
 
 const getNewGameField = () => {
   /**
-   * 1. @getCellOrder создаст массив с порядком ячеек
-   * 2. @createEmptyGameField вернет объект поле заполненное ключами и пустыми ячейками
-   * 3. @organizeGameField вернет объеет поле со стартом, фишием, стенами
-   * 4. @spreadHealthCards вернет объеет поле с разложенными карточками здоровья
+   * 1. getCellOrder создаст массив с порядком ячеек
+   * 2. createEmptyGameField вернет объект поле заполненное ключами и пустыми ячейками
+   * 3. organizeGameField вернет объеет поле со стартом, фишием, стенами
+   * 4. spreadHealthCards вернет объеет поле с разложенными карточками здоровья
+   * 5. setPlayerCards поставить карточки людей на первую позицию
    */
   const cellOrder = getCellOrder();
-
   const newEmptyGameField = createEmptyGameField(cellOrder);
   const newOrganizedGameField = organizeGameField(newEmptyGameField);
-  const healthCardsField = spreadHealthCards(newOrganizedGameField, cellOrder);
-  /* console.log(cellOrder); */
+  const healthCardsField = spreadHealthCards(newOrganizedGameField);
+  const fullPreparedGameField = setPlayerCards(healthCardsField);
+  const gameFieldWithList = { cellOrder, cellValues: fullPreparedGameField };
+  console.log("gameFieldWithList", gameFieldWithList);
 };
 
 getNewGameField();
