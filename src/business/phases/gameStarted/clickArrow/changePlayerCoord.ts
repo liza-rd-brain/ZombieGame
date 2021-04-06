86202800e436b650ddbada1c77b72afa132fc511
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-04-06 20:57:55 +0300

    Created and extracted module changePlayerCoords

---
+++ src/business/phases/gameStarted/clickArrow/changePlayerCoord.ts
@@ -0,0 +1,41 @@
+import { MoveDirection } from "../../../types";
+
+/**
+ * Returns new coordinate of player.
+ * @param currentCoord A string of current coordinate.
+ * @param direction  A string of direction.
+ * @returns A string with new coordinate.
+ */
+
+export const сhangePlayerCoord = (
+  currentCoord: string,
+  direction: MoveDirection
+) => {
+  
+  const [currPlayerHor, currPlayerVert] = currentCoord.split(".");
+  const nextPlayerVert = parseInt(currPlayerVert) + 1;
+  const nextPlayerHor = parseInt(currPlayerHor) + 1;
+  const prevPlayerVert = parseInt(currPlayerVert) - 1;
+  const prevPlayerHor = parseInt(currPlayerHor) - 1;
+
+  switch (direction) {
+    case "top": {
+      return `${currPlayerHor}.${nextPlayerVert}`;
+    }
+
+    case "bottom": {
+      return `${currPlayerHor}.${prevPlayerVert}`;
+    }
+
+    case "left": {
+      return `${prevPlayerHor}.${currPlayerVert}`;
+    }
+
+    case "right": {
+      return `${nextPlayerHor}.${currPlayerVert}`;
+    }
+
+    default:
+      return `${currPlayerHor}.${currPlayerVert}`;
+  }
+};
