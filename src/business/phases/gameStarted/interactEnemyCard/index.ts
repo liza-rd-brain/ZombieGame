75133fbb02f92b0a09319f7854f443cb3b4de7bb
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-04-08 16:45:34 +0300

    Fixed  switch in index  of module interactEnemyCard

--- src/business/phases/gameStarted/interactEnemyCard/index.ts
+++ src/business/phases/gameStarted/interactEnemyCard/index.ts
@@ -1,74 +1,67 @@
-import { State, EnemiesListType } from "../../../types";
+import { State } from "../../../types";
 
 import { ActionType } from "../../../reducer";
 import { openEnemyCard } from "./openEnemyCard";
 import { getBattleResult } from "./getBattleResult";
 
 export const interactEnemyCard = (action: ActionType, state: State): State => {
-  const enemiesCardList = state.enemiesList;
-  const playerList = state.playersList;
-  const numberOfPlayer = state.numberOfPlayer;
-  const currentCoord = playerList[numberOfPlayer].coord;
-
   switch (action.type) {
     case "checkApperanCeEnemyCard": {
-      const isNeedOpenEnemyCard =
-        enemiesCardList[currentCoord].apperance === "open" ? false : true;
-
-      switch (true) {
-        case isNeedOpenEnemyCard: {
-          return getStateNeedOpenCard(state);
-        }
-
-        case !isNeedOpenEnemyCard: {
-          return getStateTrowBattleDice(state);
-        }
-
-        default: {
-          return state;
-        }
-      }
+      return getStateCheckApperance(state);
     }
-    // TODO: просто прокинуть state
+
     case "openedEnemyCard": {
-      return getStateOpenCard(state, enemiesCardList, currentCoord);
+      return getStateOpenCard(state);
     }
 
     case "diceIsThrown": {
       const dice = action.payload;
-      return getStateDIceIsThrown(state, dice);
+      return getStateDIceIsThrown(state);
     }
 
     case "getBattleResult": {
       return getBattleResult(state);
     }
+
     default: {
       return state;
     }
   }
 };
 
-const getStateNeedOpenCard = (state: State): State => {
-  return {
-    ...state,
-    doEffect: { type: "!needOpenEnemyCard" },
-  };
+const getStateCheckApperance = (state: State): State => {
+  const { enemiesList, playersList, numberOfPlayer } = state;
+  const currentCoord = playersList[numberOfPlayer].coord;
+
+  const isNeedOpenEnemyCard =
+    enemiesList[currentCoord].apperance === "open" ? false : true;
+
+  switch (true) {
+    case isNeedOpenEnemyCard: {
+      return {
+        ...state,
+        doEffect: { type: "!needOpenEnemyCard" },
+      };
+    }
+
+    case !isNeedOpenEnemyCard: {
+      return {
+        ...state,
+        doEffect: { type: "!needThrowBattleDice" },
+        dice: 0,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
 };
 
-const getStateTrowBattleDice = (state: State): State => {
-  return {
-    ...state,
-    doEffect: { type: "!needThrowBattleDice" },
-    dice: 0,
-  };
-};
-
-const getStateOpenCard = (
-  state: State,
-  enemiesCardList: EnemiesListType,
-  currentCoord: string
-): State => {
-  const newEnemiesList = openEnemyCard(enemiesCardList, currentCoord);
+const getStateOpenCard = (state: State): State => {
+  const { enemiesList, playersList, numberOfPlayer } = state;
+  const currentCoord = playersList[numberOfPlayer].coord;
+  const newEnemiesList = openEnemyCard(enemiesList, currentCoord);
 
   return {
     ...state,
@@ -79,7 +72,8 @@ const getStateOpenCard = (
   };
 };
 
-const getStateDIceIsThrown = (state: State, dice: number): State => {
+const getStateDIceIsThrown = (state: State): State => {
+  const { dice } = state;
   return {
     ...state,
     dice: dice,
