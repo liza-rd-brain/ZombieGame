760c680e443849844025193155ed21887d2d4221
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-04-13 00:18:08 +0300

    Decomposed module getEnemies

--- src/business/initialState/getEnemies.ts
+++ src/business/initialState/getEnemies.ts
@@ -1,49 +1,39 @@
-import { EnemiesListType, GameField, CommonCell } from "../types";
+import { EnemiesListType, GameField, CommonCell, CellType } from "../types";
 
 import { AMOUNT_ENEMIES } from "../../shared/config";
 
 export const getEnemies = (gameField: GameField): EnemiesListType => {
-  //Добавить сначала рандомный массив с координатами пустых ячеек
-
-  const enemiesCoords = getEnemiesCoord(gameField);
-  console.log("enemiesCoords", enemiesCoords);
-
-  const enemiesList = enemiesCoords.map((coord) => {
-    const enemyCard = {
-      name: "enemy",
-      power: 1,
-      coord: coord,
-      apperance: "open",
-    };
-    return [enemyCard.coord, enemyCard];
-  });
-  const enemiesObj: EnemiesListType = Object.fromEntries(enemiesList);
+  const emptyCellsList = getEmptyList(gameField);
+  const enemiesCoords = getListOfIndexes(emptyCellsList);
+  const enemiesObj = getListOfEnemy(enemiesCoords);
   return enemiesObj;
 };
 
-
-
 /**
- * 1. Get list of indexes all of  empty cells
+ * For every element checks that cell has no cards on it.
+ * Returns the list of cells without any cards.
  */
-const getEnemiesCoord = (gameField: GameField) => {
-  const listFieldCells = Object.entries(gameField.values);
-
-  const emptyCellsList = listFieldCells.filter((cellItem): cellItem is [
+const getEmptyList = (gameField: GameField): [string, CommonCell][] => {
+  const listCells = Object.entries(gameField.values);
+  const emptyCellsList = listCells.filter((cellItem): cellItem is [
     string,
     CommonCell
   ] => {
     const [, item] = cellItem;
-    //check that cardItem is empty
     return (
       item.name === "commonCell" && Object.entries(item.cardItem).length === 0
     );
   });
+  return emptyCellsList;
+};
 
+/**
+ * Returns the list of random picked indexes of emptyCellsList
+ */
+const getListOfIndexes = (emptyCellsList: [string, CommonCell][]) => {
   const AMOUNT_EMPTY_CELLS = emptyCellsList.length;
-  console.log("emptyCellsList", emptyCellsList);
 
-  // TODO: it's also can be common module
+  // TODO: it may be taking out as separate module with getRandomNumber?
   const keyList: Array<number> = new Array(AMOUNT_ENEMIES)
     .fill(0)
     .reduce((prevkeyList) => {
@@ -56,18 +46,36 @@ const getEnemiesCoord = (gameField: GameField) => {
       }
     }, []);
 
-  console.log("keyList", keyList);
-
   const idexesListForCards = keyList.map((keyItem: number): string => {
     const [index] = emptyCellsList[keyItem];
     return index;
   });
 
-  console.log("listForCards", idexesListForCards);
   return idexesListForCards;
 };
 
-// TODO: take out as separete module
+/**
+ * Returns the object of all enemies
+ */
+const getListOfEnemy = (enemiesCoords: string[]) => {
+  const enemiesList = enemiesCoords.map((coord) => {
+    const enemyCard = {
+      name: "enemy",
+      power: 1,
+      coord: coord,
+      apperance: "closed",
+    };
+    return [enemyCard.coord, enemyCard];
+  });
+
+  const enemiesObj: EnemiesListType = Object.fromEntries(enemiesList);
+  return enemiesObj;
+};
+
+/**
+ * Gets an array with the previously selected random numbers and the maximum number to select.
+ * Returns a random number satisfying the conditions.
+ */
 const getRandomNumber = (
   arrNumber: Array<number>,
   maxNumber: number
