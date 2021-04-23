6ff55765f7b94f545a0c2f2729876de88c5edc6f
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-04-23 18:57:14 +0300

    Created component HealthSlot

--- src/features/PlayersStatusList.tsx
+++ src/features/PlayersStatusList.tsx
@@ -2,14 +2,10 @@ import { useSelector } from "react-redux";
 
 import styled from "styled-components";
 
-import {
-  State,
-  GameState,
-  TypeEffect,
-  PlayerListType,
-} from "../business/types";
+import { State, PlayerListType } from "../business/types";
 
 import { AMOUNT_PLAYERS, MAX_HEALTH_AMOUNT } from "../shared/config";
+import { HealthSlots } from "./HealthSlots";
 
 type HealthSlotType = {
   isFilled: boolean;
@@ -22,7 +18,7 @@ type AmountOfPlayers = {
 const PlayersListWrap = styled.div<AmountOfPlayers>`
   width: 130px;
   height: 100px;
-/*   border: 1px solid lightgray; */
+  /*   border: 1px solid lightgray; */
   display: grid;
 
   grid-template-rows: 20% auto auto;
@@ -42,72 +38,31 @@ const CharacterAvatar = styled.div`
   grid-row-end: 2;
 `;
 
-const HealthSlotList = styled.div`
-  display: flex;
-  flex-direction: column;
-  /*   height: 15px; */
-  align-items: center;
+const HealthSlotsWrap = styled.div`
   grid-row-start: 2;
   grid-row-end: 3;
-`;
-const HealthSlot = styled.div<HealthSlotType>`
-  width: 5px;
-  height: 5px;
-  border-radius: 50%;
-  border: 1px solid #f09292;
-  margin: 1px;
-  background-color: ${(props) => {
-    if (props.isFilled) {
-      return "#f09292";
-    } else return "none";
-  }};
+  & > * {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-direction: column;
+    width:100%
+  }
 `;
 
 export const PlayersStatusList = () => {
-  const { playerList, numberOfPlayer } = useSelector((state: State) => ({
-    ...state,
-  }));
-
   return (
     <PlayersListWrap amount={AMOUNT_PLAYERS}>
       {new Array(AMOUNT_PLAYERS).fill(0).map((player, index) => {
         return (
           <>
             <CharacterAvatar>{`${index + 1}`}</CharacterAvatar>
-            <HealthSlotList>
-              {getHealthSlot(playerList, index).map(
-                (healthSlotFilled: boolean) => {
-                  return <HealthSlot isFilled={healthSlotFilled}></HealthSlot>;
-                }
-              )}
-            </HealthSlotList>
+            <HealthSlotsWrap>
+              <HealthSlots index={index}></HealthSlots>
+            </HealthSlotsWrap>
           </>
         );
       })}
     </PlayersListWrap>
   );
-};
-
-const getHealthSlot = (playersList: PlayerListType, index: number) => {
-  const playerHealth = getPlayerHealth(playersList, index);
-  const maxHealthSlotList = new Array(MAX_HEALTH_AMOUNT).fill(0);
-
-  const filledHealthSlotList = maxHealthSlotList.reduce(
-    (prev, currSlot, index) => {
-      if (index < playerHealth) {
-        return [...prev, true];
-      } else {
-        return [...prev, false];
-      }
-    },
-    []
-  );
-  return filledHealthSlotList;
-};
-
-const getPlayerHealth = (
-  playersList: PlayerListType,
-  numberOfPlayer: number
-) => {
-  return playersList[numberOfPlayer].health;
 };
