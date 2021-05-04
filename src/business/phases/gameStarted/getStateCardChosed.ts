4583a3f6c92765db47b2b8dc5534856a065649ea
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-05-04 18:45:53 +0300

    Highlightning of HeathCard managed  now in local state in inventory

--- src/business/phases/gameStarted/getStateCardChosed.ts
+++ src/business/phases/gameStarted/getStateCardChosed.ts
@@ -13,7 +13,7 @@ Need switch for click false/true
 export const getStateCardChosed = (state: State, currentCardIndex: number) => {
   const stateWithHighlightning: State = {
     ...state,
-    /*  playerList: newPlayerList, */
+
     gameState: {
       type:
         state.gameState.type === "gameStarted.applyCard"
@@ -22,111 +22,4 @@ export const getStateCardChosed = (state: State, currentCardIndex: number) => {
     },
   };
   return stateWithHighlightning;
-  /* 
-  const hasAnyCardHighlightning = findAnyHighlightning(state);
-  const hasCurrentCardHighlightning = findCurrentCardHighlightning(
-    state,
-    currentCardIndex
-  );
-
-  const newPlayerList = changeHighlightningInventory(
-    state,
-    currentCardIndex,
-    hasCurrentCardHighlightning
-  );
-
-  const stateWithHighlightning: State = {
-    ...state,
-    playerList: newPlayerList,
-    gameState: {
-      type: "gameStarted.applyCard",
-    },
-  };
-
-  const stateWithoutHighlightning: State = {
-    ...state,
-    playerList: newPlayerList,
-    gameState: {
-      type: "gameStarted.playerMove",
-    },
-  };
-
-  switch (hasCurrentCardHighlightning) {
-    case true: {
-      return stateWithoutHighlightning;
-    }
-
-    case false: {
-      switch (hasAnyCardHighlightning) {
-        case false: {
-          return stateWithHighlightning;
-        }
-
-        case true: {
-          return state;
-        }
-      }
-    }
-  } */
 };
-
-/**
- * Just returns the opposite of current highlightning.
- */
-/* const changeHighlightningInventory = (
-  state: State,
-  currentCardIndex: number,
-  hasCurrentCardHighlightning: boolean
-) => {
-  const { playerList, numberOfPlayer } = state;
-  const inventory = playerList[numberOfPlayer].inventory;
-  const choosenHealthCard = inventory[currentCardIndex];
-
-  const highlightHealthCard = {
-    ...choosenHealthCard,
-    highlighting: !hasCurrentCardHighlightning,
-  };
-
-  const newInventory = inventory.map((card, index) => {
-    if (index === currentCardIndex) {
-      return highlightHealthCard;
-    } else return card;
-  });
-
-  const newPlayerList: PlayerListType = {
-    ...playerList,
-    [numberOfPlayer]: {
-      ...playerList[numberOfPlayer],
-      inventory: newInventory,
-    },
-  };
-  return newPlayerList;
-}; */
-
-/* const findAnyHighlightning = (state: State): boolean => {
-  const { playerList, numberOfPlayer } = state;
-  const inventory = playerList[numberOfPlayer].inventory;
-
-  const hasAnyCardHighlightning = inventory.find((card) => {
-    return card.highlighting === true;
-  })
-    ? true
-    : false;
-  return hasAnyCardHighlightning;
-}; */
-
-/* const findCurrentCardHighlightning = (
-  state: State,
-  currentCardIndex: number
-): boolean => {
-  const { playerList, numberOfPlayer } = state;
-  const inventory = playerList[numberOfPlayer].inventory;
-
-  const choosenHealthCard = inventory[currentCardIndex];
-
-  const hasCurrentCardHighlightning = choosenHealthCard.highlighting
-    ? true
-    : false;
-
-  return hasCurrentCardHighlightning;
-}; */
