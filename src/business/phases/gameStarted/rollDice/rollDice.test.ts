65d28e11dfe36201c8d472363a8353e10022926d
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-07-13 19:48:09 +0300

    Fixed test rollDice.

--- src/business/phases/gameStarted/rollDice/rollDice.test.ts
+++ src/business/phases/gameStarted/rollDice/rollDice.test.ts
@@ -1,13 +1,17 @@
-import { initialState } from "../../../initialState";
+import { createConfig } from "../../../../shared/helpers/createConfig";
+import { getInitialState } from "../../../initialState";
 import { reducer } from "../../../reducer";
 import { State } from "../../../types";
 
 test("should shange dice to payload,gameState.type to gameStarted.playerMove and doEffect.type to'!checkAvailableNeighboringCell '", () => {
+  const config = createConfig();
+
+  const initialState = getInitialState(config);
+
   const stateBeforeThrownDice: State = {
     ...initialState,
     gameState: {
-      coordOfAvailableCells: null,
-      coordOfAvailableCards: null,
+      ...initialState.gameState,
       type: "gameStarted.rollDice",
     },
     activePlayerNumber: 0,
