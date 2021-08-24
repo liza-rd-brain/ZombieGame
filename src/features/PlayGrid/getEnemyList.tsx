f8b592bc17a26bf63b14de18d104010a1e056592
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-08-24 17:31:22 +0300

    Fixed style of portal window when player open inventoryCard.

--- src/features/PlayGrid/getEnemyList.tsx
+++ src/features/PlayGrid/getEnemyList.tsx
@@ -12,23 +12,14 @@ export const getEnemyList = (
   deadPlayerList: DeadPlayerListType,
   activePlayerNumber: number
 ) => {
-  /*   let enemiesArr: EnemyCardType[] = []; */
-
   /**
    * Need to draw enemy if current coord of cell has the same coord
    */
-
-  const enemiesArr = Object.entries(enemiesList).map(([string, enemyCard]) => {
-    if (enemyCard.coord === index) {
-      return enemyCard;
-    }
-  });
-
   const filteredEnemiesArr = Object.entries(enemiesList).filter(
     ([string, enemyCard]) => enemyCard.coord === index
   );
 
-  if (enemiesArr.length > 0) {
+  if (filteredEnemiesArr.length > 0) {
     return (
       <EnemyList
         list={filteredEnemiesArr}
