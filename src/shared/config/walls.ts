1e49e46d248c977351d1c069d8ef3b599350e593
MinakovaLisa <33760912+MinakovaLisa@users.noreply.github.com>
2026-04-23 21:23:15 +0300

    fix: walls config + add image

--- src/shared/config/walls.ts
+++ src/shared/config/walls.ts
@@ -37,12 +37,12 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
   },
 
   {
-    coord: { hor: 2, vert: 5 },
+    coord: { hor: 2, vert: 4 },
     barrierList: [
       {
         name: "window",
         direction: "left",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
@@ -72,12 +72,12 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
   },
 
   {
-    coord: { hor: 2, vert: 8 },
+    coord: { hor: 2, vert: 7 },
     barrierList: [
       {
         name: "window",
         direction: "left",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
@@ -121,12 +121,12 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
     ],
   },
   {
-    coord: { hor: 5, vert: 10 },
+    coord: { hor: 5, vert: 9 },
     barrierList: [
       {
         name: "door",
         direction: "bottom",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
@@ -203,7 +203,7 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
       {
         name: "door",
         direction: "bottom",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
@@ -247,7 +247,7 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
       {
         name: "window",
         direction: "bottom",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
@@ -310,20 +310,21 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
     ],
   },
   {
-    coord: { hor: 4, vert: 7 },
+    coord: { hor: 4, vert: 6 },
     barrierList: [
       {
         name: "door",
         direction: "left",
-        isOpen: true,
+        isOpen: false,
       },
       {
         name: "door",
         direction: "bottom",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
+
 
   {
     coord: { hor: 5, vert: 7 },
@@ -358,12 +359,12 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
   },
 
   {
-    coord: { hor: 7, vert: 8 },
+    coord: { hor: 7, vert: 7 },
     barrierList: [
       {
         name: "door",
         direction: "left",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
@@ -377,12 +378,12 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
     ],
   },
   {
-    coord: { hor: 9, vert: 7 },
+    coord: { hor: 9, vert: 6 },
     barrierList: [
       {
         name: "door",
         direction: "bottom",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
@@ -396,17 +397,17 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
     ],
   },
   {
-    coord: { hor: 7, vert: 5 },
+    coord: { hor: 7, vert: 4 },
     barrierList: [
       {
         name: "door",
         direction: "left",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
   {
-    coord: { hor: 7, vert: 4 },
+    coord: { hor: 7, vert: 5 },
     barrierList: [
       {
         name: "wall",
@@ -435,17 +436,17 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
   },
 
   {
-    coord: { hor: 11, vert: 8 },
+    coord: { hor: 11, vert: 7 },
     barrierList: [
       {
         name: "window",
         direction: "left",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
   {
-    coord: { hor: 11, vert: 7 },
+    coord: { hor: 11, vert: 8 },
     barrierList: [
       {
         name: "wall",
@@ -463,17 +464,17 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
     ],
   },
   {
-    coord: { hor: 11, vert: 5 },
+    coord: { hor: 11, vert: 4 },
     barrierList: [
       {
         name: "window",
         direction: "left",
-        isOpen: true,
+        isOpen: false,
       },
     ],
   },
   {
-    coord: { hor: 11, vert: 4 },
+    coord: { hor: 11, vert: 5 },
     barrierList: [
       {
         name: "wall",
@@ -507,7 +508,7 @@ export const CELLS_BARRIERS_LIST: CellsBarrierListType = [
       {
         name: null,
         direction: "left",
-        isOpen: true,
+      isOpen: false,
       },
     ],
   }, */
