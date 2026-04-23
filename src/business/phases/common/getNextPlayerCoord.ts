feb5616cf255802b835b7cc407dae93856d16351
MinakovaLisa <33760912+MinakovaLisa@users.noreply.github.com>
2026-04-23 23:39:19 +0300

    some planing + temporary scenario lplayer movemrnt

--- src/business/phases/common/getNextPlayerCoord.ts
+++ src/business/phases/common/getNextPlayerCoord.ts
@@ -7,16 +7,68 @@ import { MoveDirection } from "../../types";
  * @returns A string with new coordinate.
  */
 
-export const getNextPlayerCoord = (
-  currentCoord: string,
-  direction: MoveDirection
+export const getNextPlayerCoord = ({ currentCoord, direction, dice = 1 }:
+  {
+    currentCoord: string,
+    direction: MoveDirection
+    dice?: number
+  }
 ) => {
-  
+
+
+  console.log({ direction })
+
   const [currPlayerHor, currPlayerVert] = currentCoord.split(".");
-  const nextPlayerVert = parseInt(currPlayerVert) + 1;
-  const nextPlayerHor = parseInt(currPlayerHor) + 1;
-  const prevPlayerVert = parseInt(currPlayerVert) - 1;
-  const prevPlayerHor = parseInt(currPlayerHor) - 1;
+  const nextPlayerVert = parseInt(currPlayerVert) + dice;
+  const nextPlayerHor = parseInt(currPlayerHor) + dice;
+  const prevPlayerVert = parseInt(currPlayerVert) - dice;
+  const prevPlayerHor = parseInt(currPlayerHor) - dice;
+
+  switch (direction) {
+    case "top": {
+      return `${currPlayerHor}.${nextPlayerVert}`;
+    }
+
+    case "bottom": {
+      return `${currPlayerHor}.${prevPlayerVert}`;
+    }
+
+    case "left": {
+      return `${prevPlayerHor}.${currPlayerVert}`;
+    }
+
+    case "right": {
+      return `${nextPlayerHor}.${currPlayerVert}`;
+    }
+
+    default:
+      return `${currPlayerHor}.${currPlayerVert}`;
+  }
+};
+
+/**
+ * 
+ * Написать функцию, которая будет давать все возможные координаты, 
+ * 1 - максимум ячеек, но нельзя возвращаться назад?, хранить путь? 
+ * 2- но сбрасывать путь после подобранных карточек?
+ * 
+ */
+export const getNextPlayerCoordAvailable = ({ currentCoord, direction, dice = 1 }:
+  {
+    currentCoord: string,
+    direction: MoveDirection
+    dice?: number
+  }
+) => {
+
+
+  console.log({ direction })
+
+  const [currPlayerHor, currPlayerVert] = currentCoord.split(".");
+  const nextPlayerVert = parseInt(currPlayerVert) + dice;
+  const nextPlayerHor = parseInt(currPlayerHor) + dice;
+  const prevPlayerVert = parseInt(currPlayerVert) - dice;
+  const prevPlayerHor = parseInt(currPlayerHor) - dice;
 
   switch (direction) {
     case "top": {
